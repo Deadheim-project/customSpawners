893c53a74ae49364cda231779912b44c38316513
werner <werneralexandre@outlook.com>
2021-11-18 18:21:12 -0300

    em dia

--- CustomSpawners.cs
+++ CustomSpawners.cs
@@ -1,14 +1,16 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using Jotunn.Managers;
-using System;
+using System.Linq;
 using System.IO;
 using UnityEngine;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
 
 namespace CustomSpawners
 {
     [BepInPlugin("Detalhes.CustomSpawners", "CustomSpawners", "1.0.0")]
-    [BepInProcess("valheim.exe")]
     public class CustomSpawners : BaseUnityPlugin
     {
         public const string PluginGUID = "Detalhes.CustomSpawners";
@@ -17,68 +19,104 @@ namespace CustomSpawners
         public static bool hasSpawned = false;
         public static readonly string ModPath = Path.GetDirectoryName(typeof(CustomSpawners).Assembly.Location);
 
+        public static ConfigEntry<string> SpawnAreaConfigList;
+
+
         private void Awake()
         {
-            root = SimpleJson.SimpleJson.DeserializeObject<Root>(File.ReadAllText(Path.Combine(ModPath, "customSpawners.json")));
+
+            SpawnAreaConfigList = Config.Bind("Server config", "SpawnAreaConfigList", "prefabToCopy=stone_wall_1x1;m_spawnTimer=30;m_onGroundOnly=false;m_maxTotal=10;m_maxNear=3;m_farRadius=30;m_spawnRadius=10;m_setPatrolSpawnPoint=false;m_triggerDistance=10;m_spawnIntervalSec=10;m_levelupChance=10;m_prefabName=Boar,Neck,Deer;m_nearRadius=10; | prefabToCopy=woodiron_beam;m_spawnTimer=30;m_onGroundOnly=false;m_maxTotal=10;m_maxNear=3;m_farRadius=30;m_spawnRadius=10;m_setPatrolSpawnPoint=false;m_triggerDistance=10;m_spawnIntervalSec=10;m_levelupChance=10;m_prefabName=Greydwarf;m_nearRadius=10;",
+                    new ConfigDescription("SpawnAreaConfigList", null,
+                    new ConfigurationManagerAttributes { IsAdminOnly = true }));
             harmony.PatchAll();
-        }
 
-        [HarmonyPatch(typeof(Player), "OnSpawned")]
-        internal class OnSpawned
-        {
-            private static void Postfix(Player __instance)
+            SynchronizationManager.OnConfigurationSynchronized += (obj, attr) =>
             {
-                hasSpawned = true;
-                AddClonedItems();
-            }
-        }
-
-        public static bool IsObjectDBReady(string name)
-        {
-            return PrefabManager.Instance.GetPrefab(name) != null;
+                if (attr.InitialSynchronization)
+                {
+                    Jotunn.Logger.LogMessage("Initial Config sync event received");
+                    AddClonedItems();
+                }
+                else
+                {
+                    Jotunn.Logger.LogMessage("Config sync event received");
+                    AddClonedItems();
+                }
+            };
         }
 
         public static void AddClonedItems()
         {
-            Debug.LogError("xoxota");
-            try
+            var hammer = ObjectDB.instance.m_items.FirstOrDefault(x => x.name == "Hammer");
+
+            if (!hammer)
             {
-                foreach (SpawnAreaConfig areaConfig in root.spawnAreaConfigList)
+                Debug.LogError("Custom Spawners - Hammer could not be loaded"); return;
+            }
+
+            PieceTable table = hammer.GetComponent<ItemDrop>().m_itemData.m_shared.m_buildPieces;
+
+            var list = root.GetSpawnAreaConfigs();
+            foreach (SpawnAreaConfig areaConfig in list)
+            {
+                string newName = "CS_" + string.Join("_", areaConfig.m_prefabName);
+
+                if (table.m_pieces.Exists(x => x.name == newName))
                 {
-                    if (!IsObjectDBReady(areaConfig.m_prefabName)) return;
-
-                    GameObject customSpawner = PrefabManager.Instance.CreateClonedPrefab("CS_" + areaConfig.m_prefabName, "BonePileSpawner");
-                    SpawnArea area = customSpawner.GetComponent<SpawnArea>();
-
-                    area.m_spawnTimer = areaConfig.m_spawnTimer;
-                    area.m_onGroundOnly = areaConfig.m_onGroundOnly;
-                    area.m_maxTotal = areaConfig.m_maxTotal;
-                    area.m_maxNear = areaConfig.m_maxNear;
-                    area.m_farRadius = areaConfig.m_farRadius;
-                    area.m_spawnRadius = areaConfig.m_spawnRadius;
-                    area.m_setPatrolSpawnPoint = areaConfig.m_setPatrolSpawnPoint;
-                    area.m_triggerDistance = areaConfig.m_triggerDistance;
-                    area.m_spawnIntervalSec = areaConfig.m_spawnIntervalSec;
-                    area.m_levelupChance = areaConfig.m_levelupChance;
-                    area.m_nearRadius = areaConfig.m_nearRadius;
-
-                    foreach (SpawnArea.SpawnData data in area.m_prefabs)
-                    {
-                        data.m_prefab = PrefabManager.Instance.GetPrefab(areaConfig.m_prefabName);
-                    }
-
-                    int stableHashCode = customSpawner.name.GetStableHashCode();
-                    ZNetScene.instance.m_prefabs.Add(customSpawner);
-                    ZNetScene.instance.m_namedPrefabs.Add(stableHashCode, customSpawner);
-
-                    ObjectDB.instance.m_items.Add(customSpawner);
-                    ObjectDB.instance.UpdateItemHashes();
+                    continue;
+                }
+                GameObject customSpawner = PrefabManager.Instance.CreateClonedPrefab(newName, areaConfig.name);
+                if (customSpawner == null)
+                {
+                    Debug.LogError("nao achei porrra");
+                    continue;
                 }
 
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex.Message);
+                customSpawner.GetComponent<ZNetView>().m_syncInitialScale = true;
+
+                SpawnArea area = customSpawner.AddComponent<SpawnArea>();
+
+                Piece piece = customSpawner.GetComponent<Piece>();
+                if(!piece) customSpawner.AddComponent<Piece>();
+
+                piece.m_description = "Spawner ";
+                piece.name = customSpawner.name;
+
+                Object.Destroy(customSpawner.GetComponent<Destructible>());
+                Object.Destroy(customSpawner.GetComponent<WearNTear>());
+
+                area.m_spawnTimer = areaConfig.m_spawnTimer;
+                area.m_onGroundOnly = areaConfig.m_onGroundOnly;
+                area.m_maxTotal = areaConfig.m_maxTotal;
+                area.m_maxNear = areaConfig.m_maxNear;
+                area.m_farRadius = areaConfig.m_farRadius;
+                area.m_spawnRadius = areaConfig.m_spawnRadius;
+                area.m_setPatrolSpawnPoint = areaConfig.m_setPatrolSpawnPoint;
+                area.m_triggerDistance = areaConfig.m_triggerDistance;
+                area.m_spawnIntervalSec = areaConfig.m_spawnIntervalSec;
+                area.m_levelupChance = areaConfig.m_levelupChance;
+                area.m_nearRadius = areaConfig.m_nearRadius;
+                area.m_prefabs = new List<SpawnArea.SpawnData>();
+
+                foreach (string prefab in areaConfig.m_prefabName)
+                {
+                    var newArea = new SpawnArea.SpawnData();
+                    newArea.m_weight = 100 / areaConfig.m_prefabName.Count;
+                    newArea.m_minLevel = 1;
+                    newArea.m_maxLevel = 3;
+                    newArea.m_prefab = PrefabManager.Instance.GetPrefab(prefab);
+                    piece.m_description += prefab + " ";
+                    if (newArea.m_prefab == null) continue;
+
+                    area.m_prefabs.Add(newArea);
+                }
+
+                PieceManager.Instance.RegisterPieceInPieceTable(customSpawner, "Hammer", "Custom Spawner");
+
+                if (!SynchronizationManager.Instance.PlayerIsAdmin)
+                {
+                    table.m_pieces.Remove(customSpawner);
+                }
             }
         }
     }
--- SpawnAreaConfig.cs
+++ SpawnAreaConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CustomSpawners
 {
     public class SpawnAreaConfig
     {
+        public string name { get; set; }
         public int m_spawnTimer { get; set; }
         public bool m_onGroundOnly { get; set; }
         public int m_maxTotal { get; set; }
@@ -14,12 +16,37 @@ namespace CustomSpawners
         public int m_triggerDistance { get; set; }
         public int m_spawnIntervalSec { get; set; }
         public int m_levelupChance { get; set; }
-        public string m_prefabName { get; set; }
+        public List<string> m_prefabName { get; set; }
         public int m_nearRadius { get; set; }
     }
 
     public class Root
-    {
-        public List<SpawnAreaConfig> spawnAreaConfigList { get; set; }
+    {       
+        public List<SpawnAreaConfig> GetSpawnAreaConfigs()
+        {
+            List<SpawnAreaConfig> SpawnAreaConfigList = new List<SpawnAreaConfig>();
+            foreach (string str in CustomSpawners.SpawnAreaConfigList.Value.Trim(' ').Split('|'))
+            {
+                var areaInfo = str.Split(';');
+                var cfg = new SpawnAreaConfig();
+                cfg.name = areaInfo[0].Split('=')[1];
+                cfg.m_spawnTimer = System.Convert.ToInt32(areaInfo[1].Split('=')[1]);
+                cfg.m_onGroundOnly = bool.Parse(areaInfo[2].Split('=')[1]);
+                cfg.m_maxTotal = System.Convert.ToInt32(areaInfo[3].Split('=')[1]);
+                cfg.m_maxNear   = System.Convert.ToInt32(areaInfo[4].Split('=')[1]);
+                cfg.m_farRadius  = System.Convert.ToInt32(areaInfo[5].Split('=')[1]);
+                cfg.m_spawnRadius = System.Convert.ToInt32(areaInfo[6].Split('=')[1]);
+                cfg.m_setPatrolSpawnPoint = bool.Parse(areaInfo[7].Split('=')[1]);
+                cfg.m_triggerDistance = System.Convert.ToInt32(areaInfo[8].Split('=')[1]);
+                cfg.m_spawnIntervalSec = System.Convert.ToInt32(areaInfo[9].Split('=')[1]);
+                cfg.m_levelupChance = System.Convert.ToInt32(areaInfo[10].Split('=')[1]);
+                cfg.m_prefabName = areaInfo[11].Split('=')[1].Split(',').ToList();
+                cfg.m_nearRadius = System.Convert.ToInt32(areaInfo[12].Split('=')[1]);
+
+                SpawnAreaConfigList.Add(cfg);
+            }
+
+            return SpawnAreaConfigList;
+        }
     }
 }
